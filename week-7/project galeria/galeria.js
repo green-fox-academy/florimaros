b3a528ef65e256ea6aea10295904c18e901d7b47
flori maros <flori.maros.adw@gmail.com>
2016-01-08 11:58:49 +0100

    project_galery

--- week-7/project galeria/galeria.js
+++ week-7/project galeria/galeria.js
@@ -3,6 +3,7 @@ var list=[
   "galery1.jpg",
   "galery2.jpg",
   "galery3.jpg",
+  "galery4.jpg",
   "galery5.jpg",
   "galery6.jpg",
   "galery7.jpg"
@@ -14,24 +15,24 @@ var i = 0
 prevbutton.addEventListener('click', function() {
   i = i - 1;
   if (i > 0 && i < list.length-1) {
-  picture.setAttribute("src", list[i])
+    picture.setAttribute("src", list[i])
   }
   else {
-  i = list.length - 1
-  picture.setAttribute("src", list[i])
-    }
+    i = list.length - 1
+    picture.setAttribute("src", list[i])
+  }
 
 });
 
 nextbutton.addEventListener('click', function() {
   i = i + 1;
-  if (i > 0 && i < list.length+1) {
-  picture.setAttribute("src", list[i])  
+  if (i > 0 && i < list.length) {
+    picture.setAttribute("src", list[i])
   }
 
   else {
     i = 0
     picture.setAttribute("src", list[i])
-    }
+  }
 
 });
