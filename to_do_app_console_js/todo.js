08ec91f5b041045fb100927967655ae55c88e078
florimaros <flori.maros@yahoo.com>
2016-02-10 20:44:43 +0100

    to do ap before refactoring

--- to_do_app_console_js/todo.js
+++ to_do_app_console_js/todo.js
@@ -2,17 +2,19 @@
 //install: npm install --save readline-sync usage:
 //After printing the menu it should get an input from the command line and if the input is one of the menu
 //items number it should print the name of the menu, if not it should require the input again
+"use strict";
 
 var readlineSync = require('readline-sync');
 var menuitems = ["List todos", "Add todo", "Remove todo", "Quit"];
 var list_of_todos = ["Buy beer", "Wash the car", "Walk the dog"];
+//hogy kell js-ben fileba irni?
 
-function menu () {
+function menu() {
   console.log ("Main menu:\n");
-    for(var i=0; i<menuitems.length; i++) {
-      console.log(i+1 + ". " +  menuitems[i]);
-    }
-    console.log ("\nPlease choose a menu item: ");
+  for(var i=0; i<menuitems.length; i++) {
+    console.log(i+1 + ". " +  menuitems[i]);
+  }
+  console.log ("\nPlease choose a menu item: ");
 }
 
 function getMenuitems(index) {
@@ -20,19 +22,18 @@ function getMenuitems(index) {
 }
 
 function inputNumber() {
-  var choosenumber =  parseInt(readlineSync.question());
+  var choosenumber = parseInt(readlineSync.question());
 
   if(choosenumber >= 1 && choosenumber <= menuitems.length) {
     console.log(getMenuitems(choosenumber));
-      if(choosenumber===2) {
-        console.log("Please enter a new todo item:");
-        var todoit =  (readlineSync.question());
-        list_of_todos.push(todoit);
-        console.log("Todo item is saved");
-        menu();
-        inputNumber();
+    if(choosenumber===2) {
+      console.log("Please enter a new todo item:");
+      var todoit = readlineSync.question();
+      list_of_todos.push(todoit);
+      console.log("Todo item is saved");
+      menu();
+      inputNumber();
     }
-
     else if(choosenumber===1) {
       console.log("List of todo items:\n");
       for(var i=0; i<list_of_todos.length; i++) {
@@ -41,27 +42,25 @@ function inputNumber() {
       menu();
       inputNumber();
     }
-//indentalast atnezni, menut at tudod-e alakitani, h lehet az input no rovidebb, reszeket
     else if(choosenumber===3) {
-      var deletenumber =  parseInt(readlineSync.question());
+      var deletenumber = parseInt(readlineSync.question());
       var new_list_of_todos = [];
       for(var i=0; i<list_of_todos.length; i++) {
-        if( i !== deletenumber)
-        new_list_of_todos.push(list_of_todos[i])
+        if( i !== deletenumber-1) {
+          new_list_of_todos.push(list_of_todos[i])
+        }
       }
       list_of_todos = new_list_of_todos;
-
       menu();
       inputNumber();
     }
+    else if(choosenumber===4) {
+  //    console.log(menuitems[3]);
+    }
     else {
-    console.log("wrong input try again"); {
+    console.log("wrong input try again");
     }
   }
-  }
 }
-
-
-
 menu();
 inputNumber();
